b5eceffe5df24b7e3c7356a3b99c9f3198fa2176
MinSu Kim <ms001118@gmail.com>
2021-07-22 22:58:04 +0900

    first commit

---
+++ etc/doubleArraySanil.java
@@ -0,0 +1,31 @@
+package etc;
+
+import java.util.*;
+import java.io.*;
+
+public class doubleArraySanil {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] field = new int[N][N];
+        int counter = 1;
+
+
+
+        for(int i = 0; i < N - 1; i++) {
+            for(int j = 0; j < 2; j++) {
+                
+            }
+        }
+        
+        bw.write("done");
+        bw.flush();
+        bw.close();
+
+
+    }
+
+}
