199ac0c21fc94a07f228e119ac33c0b50c34fb8f
MinSu <ms001118@gmail.com>
2021-08-02 21:16:32 +0900

    save

--- baekjoon/no11729/Main.java
+++ baekjoon/no11729/Main.java
@@ -1,4 +1,5 @@
 package baekjoon.no11729;
+
 import java.util.ArrayList;
 //TODO: not solved
 import java.util.Scanner;
@@ -8,47 +9,94 @@ public class Main {
     static int i1 = -1;
     static int i2 = -1;
     static int i3 = -1;
-    static int[] ary1, ary2, ary3;
+    static int[] stk1, stk2, stk3;
     static ArrayList<String> aryLst = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        sc.close();
 
-        ary1 = new int[N];
-        ary2 = new int[N];
-        ary3 = new int[N];
+        stk1 = new int[N];
+        stk2 = new int[N];
+        stk3 = new int[N];
 
-        for(int i = 0; i < N; i++) {
-            ary1[++i1] = N-i;
+        for (int i = 0; i < N; i++) {
+            stk1[++i1] = N - i;
         }
 
-        hanoi(N);
+        hanoi(N, 1, 3, 2);
     }
 
-    public static void hanoi(int n) {
-        if(!canMake(n)) {
-            hanoi(n-1);
+    public static void hanoi(int n, int start, int end, int left) {
+        if (canMove(n, start, end)) {
+            move(n, start, end, left);
+            hanoi(n + 1, start, left, end);
         }
+        hanoi(n - 1, start, left, end);
     }
 
-    public static boolean canMake(int n) {
-        for(int i = 0; i <= i1; i++) {
-            if(ary1[i] == n && i == i1) {
-                return true;
-            }
+    public static boolean canMove(int n, int start, int end) {
+        boolean a = false;
+
+        switch (start) {
+            case 1:
+                if (stk1[i1] == n) {
+                    a = true;
+                }
+                break;
+            case 2:
+                if (stk2[i2] == n) {
+                    a = true;
+                }
+                break;
+            case 3:
+                if (stk3[i3] == n) {
+                    a = true;
+                }
+                break;
         }
-        for(int i = 0; i <= i2; i++) {
-            if(ary2[i] == n && i == i2) {
-                return true;
-            }
-        }
-        for(int i = 0; i <= i2; i++) {
-            if(ary3[i] == n && i == i3) {
-                return true;
-            }
+        switch (end) {
+            case 1:
+                if (i1 == -1 || stk1[i1] < n)
+                    return a;
+                break;
+            case 2:
+                if (i2 == -1 || stk2[i2] < n)
+                    return a;
+                break;
+            case 3:
+                if (i3 == -1 || stk3[i3] < n)
+                    return a;
+                break;
         }
         return false;
     }
 
-    
+    public static void move(int n, int start, int end, int left) {
+        int a = 0;
+        System.out.println(start + " " + end);
+        switch (start) {
+            case 1:
+                a = stk1[i1--];
+                break;
+            case 2:
+                a = stk2[i2--];
+                break;
+            case 3:
+                a = stk3[i3--];
+                break;
+        }
+        switch (end) {
+            case 1:
+                stk1[++i1] = a;
+                break;
+            case 2:
+                stk2[++i2] = a;
+                break;
+            case 3:
+                stk3[++i3] = a;
+                break;
+        }
+    }
 }
