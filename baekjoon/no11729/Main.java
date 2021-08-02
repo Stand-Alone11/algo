61206125b44a87659c9d71637e96396b2001a96c
MinSu Kim <ms001118@gmail.com>
2021-08-02 09:26:12 +0900

    hanoi solving

--- baekjoon/no11729/Main.java
+++ baekjoon/no11729/Main.java
@@ -8,14 +8,15 @@ public class Main {
     static int i1 = -1;
     static int i2 = -1;
     static int i3 = -1;
+    static int[] ary1, ary2, ary3;
     static ArrayList<String> aryLst = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int[] ary1 = new int[N];
-        int[] ary2 = new int[N];
-        int[] ary3 = new int[N];
+        ary1 = new int[N];
+        ary2 = new int[N];
+        ary3 = new int[N];
 
         for(int i = 0; i < N; i++) {
             ary1[++i1] = N-i;
@@ -25,10 +26,28 @@ public class Main {
     }
 
     public static void hanoi(int n) {
-        count++;
+        if(!canMake(n)) {
+            hanoi(n-1);
+        }
+    }
 
-        
-        
+    public static boolean canMake(int n) {
+        for(int i = 0; i <= i1; i++) {
+            if(ary1[i] == n && i == i1) {
+                return true;
+            }
+        }
+        for(int i = 0; i <= i2; i++) {
+            if(ary2[i] == n && i == i2) {
+                return true;
+            }
+        }
+        for(int i = 0; i <= i2; i++) {
+            if(ary3[i] == n && i == i3) {
+                return true;
+            }
+        }
+        return false;
     }
 
     
